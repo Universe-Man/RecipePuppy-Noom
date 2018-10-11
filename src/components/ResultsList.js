4b5bf1e2989e33757a7fb9dfe237fbb9699ec479
github username <ianjpollack@gmail.com>
2018-10-11 17:53:26 -0400

    timeOut and fetch

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,26 +1,31 @@
 import React from 'react';
 
-class ResultsList extends React.Component {
-  constructor() {
-    super();
-    this.state = {}
-  }
+const ResultsList = (props) => {
 
-  render() {
+  console.log("this is props in the results list", props.displayedRecipes);
+  // displayStuff = () => {
+  //
+  // }
+  if (props.displayedRecipes === []) {
     return(
       <div id="ResultsList">
         <ol id="RL-OL">
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          {/*<li><a href="#">Cindy</a></li>*/}
+        </ol>
+      </div>
+    )
+  } else {
+    for (let i = 0; i < props.displayedRecipes.length; i++){
+      console.log("in the loop", i); 
+    }
+    return(
+      <div id="ResultsList">
+        <ol id="RL-OL">
+          {props.displayedRecipes}
         </ol>
       </div>
     )
   }
 }
+
 
 export default ResultsList;
--- src/containers/MasterContainer.js
+++ src/containers/MasterContainer.js
@@ -7,13 +7,38 @@ class MasterContainer extends React.Component {
     super();
     this.state = {
       searchTerm: "",
+      allRecipes: [],
+      displayedRecipes: ["a", "b", "c"],
     }
   }
+
+  componentDidMount() {
+    fetch("https://cors-anywhere.herokuapp.com/http://www.recipepuppy.com/api/")
+      .then(res => res.json())
+      .then(json => this.setState({
+        allRecipes: json.results}, () => console.log("goo", this.state.allRecipes)))
+  }
+
+  filterRecipes = () => {
+    console.log('fetching...');
+    // WRITTEN TO ATTEMPT A LOCAL CHECK INSTEAD OF HITTING TH API EVERYTIME
+    // let searchWord = this.state.searchTerm
+    // debugger
+    // if (this.state.allRecipes.includes(searchWord)) {
+    //   let filteredRecipes = this.state.allRecipes.filter(recipe => recipe.title.toLowerCase().includes(searchWord.toLowerCase()))
+    //   this.setState({
+    //     displayedRecipes: filteredRecipes
+    //   })
+    // } else {
+      // console.log("gotta fetch it from the API");
+    // }
+
+
 
   getSearchTerm = (event) => {
     this.setState({
       searchTerm: event.target.value
-    })
+    }, () => {setTimeout(this.filterRecipes, 500)})
   }
 
   render() {
@@ -21,7 +46,7 @@ class MasterContainer extends React.Component {
       <div id="MasterContainer">
         <h1>WELCOME TO RECIPE PUPPY!!</h1>
         <SearchBar getSearchTerm={this.getSearchTerm} />
-        <ResultsList />
+        <ResultsList displayedRecipes={this.state.displayedRecipes}/>
 
 
 
